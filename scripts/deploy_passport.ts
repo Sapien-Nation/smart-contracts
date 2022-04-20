e5ffbe40d96a81f52b83af1574b5960e655d1e1d
shiro <shiro@sapien.network>
2022-04-20 22:46:18 +0900

    Deploy passport(testing) to mainnet

--- scripts/deploy_passport.ts
+++ scripts/deploy_passport.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from 'hardhat';
 
 async function main() {
   const Passport = await ethers.getContractFactory('Passport');
-  const passport = await upgrades.deployProxy(Passport, ['Sapien Passport NFT', 'SPASS', 'https://sapien.network/metadata/passport/', '0xBac27Ed5d503b2F40C76Ff0c747bF55C753433DA']);
+  const passport = await upgrades.deployProxy(Passport, ['Sapien Passport NFT', 'SPASS', 'ipfs://', '0xb8CC7C13c47731d4c7970Af884923E5E41b97551']);
   await passport.deployed();
   console.log('Passport deployed to:', passport.address);
 }
--- scripts/deploy_role_manager.ts
+++ scripts/deploy_role_manager.ts
@@ -1,8 +1,12 @@
 import { ethers } from 'hardhat';
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  
   const RoleManager = await ethers.getContractFactory('RoleManager');
-  const roleManager = await RoleManager.deploy('');
+  const roleManager = await RoleManager.deploy();
 
   console.log('Role manager contract deployed to:', roleManager.address);
 }
