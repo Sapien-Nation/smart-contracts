4144f813404947123622f88003c9c1c4358551ba
shiro <shiro@sapien.network>
2022-04-27 00:32:19 +0900

    Deploy to mainnet

--- scripts/deploy_passport.ts
+++ scripts/deploy_passport.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from 'hardhat';
 
 async function main() {
   const Passport = await ethers.getContractFactory('Passport');
-  const passport = await upgrades.deployProxy(Passport, ['Sapien Nation Passport', 'SNP', 'ipfs://', '0xb8CC7C13c47731d4c7970Af884923E5E41b97551', '0x86C80a8aa58e0A4fa09A69624c31Ab2a6CAD56b8']);
+  const passport = await upgrades.deployProxy(Passport, ['Sapien Nation Passport', 'SNP', 'ipfs://', '0x34E8b95cD75e3b14447410F0038dF4BA42512543', '0x86C80a8aa58e0A4fa09A69624c31Ab2a6CAD56b8']);
   await passport.deployed();
   console.log('Passport deployed to:', passport.address);
 }
