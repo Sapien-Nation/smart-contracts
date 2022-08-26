2f18ab13665bc5bdc193671ac3e927c7db1b24cb
mateo <mateo@sapien.network>
2022-08-26 08:09:17 -0700

    create new badge contract to get an early access to a tribe

---
+++ contracts/EarlyAccessTribeBadge.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+
+contract EarlyAccessTribeBadge is ERC1155, Ownable, Pausable, ERC2771Context {
+	// Latest badge id starting from 1
+	uint256 public badgeID;
+  // Biconomy forwarder contract address
+  address public trustedForwarder;
+	
+  event LogURISet(string uri);
+  event LogTrustedForwarderSet(address indexed trustedForwarder);
+	event LogMint(address indexed account, uint256 tokenID);
+	event LogCreate(uint256 badgeID);
+  
+	constructor(
+		string memory _uri, 
+    address _trustedForwarder
+	) ERC1155(_uri) ERC2771Context(_trustedForwarder) {
+    _setTrustedForwarder(_trustedForwarder);
+	}
+
+  /** 
+	 * @dev Set contract URI
+	 * Accessible by only contract owner
+	 */
+  function setURI(string memory _uri) external onlyOwner {
+    require(bytes(_uri).length > 0, "TribeBadge: EMPTY_STRING");
+    super._setURI(_uri);
+
+    emit LogURISet(_uri);
+  }
+
+	/**
+	 * @dev Mint new badges
+	 * Only contract owner(Sapien Governance wallet) can access to this mintBatch method
+	 * Length of `_accounts` and `_tokenIDs` must be the same
+   * Each account must not have more than 1 badge
+	 */
+	function mintBatch(
+		address[] calldata _accounts,
+		uint256[] calldata _tokenIDs
+	) external onlyOwner {
+		require(_accounts.length == _tokenIDs.length, "TribeBadge: ARRAY_LENGTH_MISMATCH");
+    
+    for(uint256 i = 0; i < _tokenIDs.length; i++) {
+      require(0 < _tokenIDs[i] && _tokenIDs[i] <= badgeID, "TribeBadge: TOKEN_ID_INVALID");
+      require(balanceOf(_accounts[i], _tokenIDs[i]) == 0, "TribeBadge: TOKEN_ALREADY_OWN");
+      super._mint(_accounts[i], _tokenIDs[i], 1, "");
+
+      emit LogMint(_accounts[i], _tokenIDs[i]);
+    }
+	}
+  
+  /**
+   * @dev Create new badge
+   * Accessible by only contract owner
+   */
+  function createBadge() external onlyOwner {
+    uint256 newBadgeID = badgeID + 1;
+		badgeID = newBadgeID;
+		emit LogCreate(newBadgeID);
+  }
+
+  function _beforeTokenTransfer(
+    address operator,
+    address from,
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) internal override {
+    require(
+      from == address(0) && to != address(0),
+      "Error: token is not transferable and burnable"
+    );
+    super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+  }
+  
+  /** 
+	 * @dev Set `trustedForwarder` address
+	 * Accessible by only contract owner
+	 */
+  function setTrustedForwarder(address _trustedForwarder) external onlyOwner {
+    _setTrustedForwarder(_trustedForwarder);
+  }
+
+  /**
+   * @dev Override {ERC2771Context-isTrustedForwarder}
+   * Compare with `trustedForwarder`
+   * We want flexibility to update biconomy forwarder address
+   */
+  function isTrustedForwarder(address _forwarder) public view override returns (bool) {
+    return _forwarder == trustedForwarder;
+  }
+
+  function _setTrustedForwarder(address _trustedForwarder) private {
+    require(_trustedForwarder != address(0), "TribeBadge: ZERO_ADDRESS");
+    trustedForwarder = _trustedForwarder;
+
+    emit LogTrustedForwarderSet(_trustedForwarder);
+  }
+
+  /**
+    * @dev Override {ERC2771Context-_msgSender}
+    */
+  function _msgSender() internal view virtual override(Context, ERC2771Context) returns (address) {
+    return ERC2771Context._msgSender();
+  }
+
+  /**
+    * @dev Override {ERC2771Context-_msgData}
+    */
+  function _msgData() internal view virtual override(Context, ERC2771Context) returns (bytes calldata) {
+    return ERC2771Context._msgData();
+  }
+}
---
+++ test/EarlyAccessTribeBadge.test.ts
@@ -0,0 +1,76 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+
+async function setup() {
+  const [owner, alice, bob, carol, darren, eric] = await ethers.getSigners();
+  const trustedForwarder:string = '0xeF60a8E421639Fc8A63b98118c5b780579b1009A';
+  const TribeBadge = await ethers.getContractFactory('EarlyAccessTribeBadge');
+  const tribeBadge = await TribeBadge.deploy('https://sapien.network/badges/{id}.json', trustedForwarder);
+  await tribeBadge.deployed();
+  return {tribeBadge, owner, alice, bob, carol, darren, eric};
+}
+
+describe('TribeBadge', async () => {
+	let tribeBadge: any;
+	let owner: any, alice: any, bob: any, carol: any, darren: any, eric: any;
+	
+	describe('CreateBadge', async () => {
+		it('expect to create new badge', async () => {
+			({tribeBadge, owner, alice, bob, carol, darren, eric} = await setup());
+			await tribeBadge.createBadge();
+			expect((await tribeBadge.badgeID()).toString()).to.eq('1');
+		});
+		describe('reverts if', async () => {
+			it('caller is not owner', async () => {
+				await expect(tribeBadge.connect(alice).createBadge())
+					.to.be.revertedWith('Ownable: caller is not the owner');
+			});
+		});
+	});
+
+	describe('MintBatch', async () => {
+		it('expect to mint batch tokens', async () => {
+			// owner call
+			await tribeBadge.mintBatch([darren.address], [1]);
+			expect((await tribeBadge.balanceOf(darren.address, 1)).toString()).to.eq('1');
+		});
+    describe('reverts if', async () => {
+      it('tries to mint badges not created', async () => {
+        await expect((tribeBadge.mintBatch([bob.address, carol.address], [2, 0])))
+          .to.be.revertedWith('TribeBadge: TOKEN_ID_INVALID');
+      });
+      it('array params length mismatch', async () => {
+        await expect((tribeBadge.mintBatch([bob.address, carol.address], [2, 2, 2])))
+          .to.be.revertedWith('TribeBadge: ARRAY_LENGTH_MISMATCH');
+      });
+      it('accounts already own at least 1 badge', async () => {
+        await expect(tribeBadge.mintBatch([darren.address], [1]))
+          .to.be.revertedWith('TribeBadge: TOKEN_ALREADY_OWN');
+      });
+    });
+	});
+
+  describe('Setters for URI', async () => {
+    it('expect to set new URI', async () => {
+      await tribeBadge.setURI('https://sapien.network/badges/updated/{id}.json');
+      expect(await tribeBadge.uri(1)).to.eq('https://sapien.network/badges/updated/{id}.json');
+    });
+    describe('reverts if', async () => {
+      it('setURI() caller is not owner', async () => {
+        await expect(tribeBadge.connect(alice).setURI('https://sapien.network/badges/updated/{id}.json'))
+          .to.be.revertedWith('Ownable: caller is not the owner');
+      });
+      it('new URI is empty string', async () => {
+        await expect(tribeBadge.setURI(''))
+          .to.be.revertedWith('TribeBadge: EMPTY_STRING');
+      });
+    });
+  });
+
+  describe('Non transferable', async () => {
+    it('expect to badge transfer disabled', async () => {
+      await expect(tribeBadge.connect(darren).safeTransferFrom(darren.address, alice.address, 1, 1, ethers.constants.HashZero))
+        .to.be.revertedWith('Error: token is not transferable and burnable');
+    });
+  })
+});
